b8ebae89fb8ec4283ae14640b09ced8b881498aa
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-08-09 20:50:21 +0000

    Initial commit & Session Controller

---
+++ backend/app/Controllers/Http/SessionController.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class SessionController {
+    async register ({ request }) {
+        const data = request.only(["username", "email", "password"]);
+        
+        const user = await User.create(data);
+        
+        return user;
+    }
+    
+    async login({ request, auth }) {
+        const { email, password } = request.all();
+    
+        const token = await auth.attempt(email, password);
+    
+        return token;
+    }
+}
+
+module.exports = SessionController
