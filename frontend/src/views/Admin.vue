dfd0e1e91d3cc6084335c05b9d9673710f8bfdcc
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-08-16 17:39:11 +0000

    Finish Admin Page

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -3,81 +3,131 @@
         <AdminHeader />
         <v-container>
             <h1>Painel Administrativo</h1>
-            <v-dialog v-model="dialog" max-width="500px" color="secondary">
-              <v-btn slot="activator" color="secondary" dark><v-icon>add</v-icon> Adicionar Usuário</v-btn>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">Adicionar Usuário</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container grid-list-md>
-                    <v-layout wrap>
-                      <v-flex xs12>
-                        <v-text-field 
-                            label="Usuário *"
-                            hint="Nome completo"
-                            required
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12>
-                        <v-text-field 
-                            label="Email *" 
-                            hint="E-mail de compra"
-                            required
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12>
-                        <v-text-field 
-                            label="Senha *" 
-                            type="password" 
-                            required
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12>
-                        <v-checkbox
-                          v-model="adminCB"
-                          label="Administrador"
-                          color="secondary"
-                          value="admin"
-                          hide-details
-                        ></v-checkbox>
-                      </v-flex>
-                      <v-flex xs12>
-                        <v-autocomplete
-                          :items="['Ortodontia Iniciante I', 'Ortodontia Iniciante II', 'Ortodontia Intermediária I', 'Ortodontia Avançada I', 'Ortodontia Avançada IV']"
-                          label="Cursos *"
-                          multiple
-                          chips
-                        ></v-autocomplete>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                  <small>* Campos obrigatórios</small>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="error" @click.native="dialog = false">Fechar</v-btn>
-                  <v-btn color="secondary" @click.native="dialog = false">Salvar</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-            <v-layout>
-                <v-flex>
-                    <v-data-table
-                        :headers="headers"
-                        :items="users"
-                        class="elevation-1"
-                    >
-                    <template slot="items" slot-scope="props">
-                      <td>{{ props.item.id }}</td>
-                      <td>{{ props.item.name }}</td>
-                      <td>{{ props.item.email }}</td>
-                      <td>{{ props.item.admin }}</td>
-                      <td>{{ props.item.cursos }}</td>
-                    </template>
-                  </v-data-table>
-                </v-flex>
-            </v-layout>
+            <div class="users-table">
+              <div class="table-actions">
+                <h2>Tabela de Usuários</h2>
+                <v-dialog v-model="userDialog" max-width="500px">
+                    <v-btn slot="activator" color="secondary"><v-icon>add</v-icon> Adicionar Usuário</v-btn>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">Adicionar Usuário</span>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-container grid-list-md>
+                          <v-layout wrap>
+                            <v-flex xs12>
+                              <v-text-field 
+                                  label="Usuário *"
+                                  hint="Nome completo"
+                                  required
+                              ></v-text-field>
+                            </v-flex>
+                            <v-flex xs12>
+                              <v-text-field 
+                                  label="Email *" 
+                                  hint="E-mail de compra"
+                                  required
+                              ></v-text-field>
+                            </v-flex>
+                            <v-flex xs12>
+                              <v-text-field 
+                                  label="Senha *" 
+                                  type="password" 
+                                  required
+                              ></v-text-field>
+                            </v-flex>
+                            <v-flex xs12>
+                              <v-checkbox
+                                v-model="adminCB"
+                                label="Administrador"
+                                color="secondary"
+                                value="admin"
+                                hide-details
+                              ></v-checkbox>
+                            </v-flex>
+                            <v-flex xs12>
+                              <v-autocomplete
+                                :items="['Ortodontia Iniciante I', 'Ortodontia Iniciante II', 'Ortodontia Intermediária I', 'Ortodontia Avançada I', 'Ortodontia Avançada IV']"
+                                label="Cursos *"
+                                multiple
+                                chips
+                              ></v-autocomplete>
+                            </v-flex>
+                          </v-layout>
+                        </v-container>
+                        <small>* Campos obrigatórios</small>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="error" @click.native="userDialog = false">Fechar</v-btn>
+                        <v-btn color="secondary" @click.native="userDialog = false">Salvar</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+              </div>
+                <v-layout>
+                    <v-flex>
+                        <v-data-table
+                            :headers="usersHeaders"
+                            :items="users"
+                        >
+                        <template slot="items" slot-scope="props">
+                          <td>{{ props.item.id }}</td>
+                          <td>{{ props.item.name }}</td>
+                          <td>{{ props.item.email }}</td>
+                          <td>{{ props.item.admin }}</td>
+                          <td>{{ props.item.cursos }}</td>
+                        </template>
+                      </v-data-table>
+                    </v-flex>
+                </v-layout>
+            </div>
+            
+            <div class="courses-table">
+              <div class="courses-actions">
+                <h2>Tabela de Cursos</h2>
+                <v-dialog v-model="courseDialog" max-width="500px">
+                    <v-btn slot="activator" color="secondary"><v-icon>add</v-icon> Adicionar Curso</v-btn>
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">Adicionar Curso</span>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-container grid-list-md>
+                          <v-layout wrap>
+                            <v-flex xs12>
+                              <v-text-field 
+                                  label="Nome do curso *"
+                                  required
+                              ></v-text-field>
+                            </v-flex>
+                          </v-layout>
+                        </v-container>
+                        <small>* Campos obrigatórios</small>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="error" @click.native="courseDialog = false">Fechar</v-btn>
+                        <v-btn color="secondary" @click.native="courseDialog = false">Salvar</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+              </div>
+              <v-layout>
+                    <v-flex>
+                        <v-data-table
+                            :headers="coursesHeaders"
+                            :items="courses"
+                            class="elevation-1"
+                        >
+                        <template slot="items" slot-scope="props">
+                          <td>{{ props.item.id }}</td>
+                          <td>{{ props.item.name }}</td>
+                        </template>
+                      </v-data-table>
+                    </v-flex>
+                </v-layout> 
+            </div>
         </v-container>
     </div>
 </template>
@@ -92,7 +142,7 @@ export default {
     },
     data () {
       return {
-        headers: [
+        usersHeaders: [
           { text: 'id', value: 'id' },
           { text: 'Usuário', value: 'name' },
           { text: 'E-mail', sortable: false, value: 'email' },
@@ -108,7 +158,18 @@ export default {
             cursos: '1, 2',
           },
         ],
-        dialog: false,
+        coursesHeaders: [
+          { text: 'id', value: 'id' },
+          { text: 'Nome', value: 'name' },
+        ],
+        courses: [
+          {
+            id: "1",
+            name: 'Ortodontia Iniciante I',
+          },
+        ],
+        userDialog: false,
+        courseDialog: false,
         adminCB: false,
       };
     }
@@ -122,10 +183,27 @@ h1 {
 }
 
 .v-btn {
-    margin: 10px 0 20px 0;
+    margin: 20px 10px 20px 0;
+    float: right;
 }
 
 .v-icon {
     margin-right: 5px;
+}
+
+.users-table, .courses-table {
+  margin-top: 15px;
+  border: 1px solid #e5e5e5;
+  background: #d1d8e0;
+}
+
+.table-actions, .courses-actions {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h2 {
+  margin-left: 10px;
 }
 </style>
