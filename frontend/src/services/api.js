39316409f1f7abe8084ede82f6655e91bc54df5f
brunogcpinheiro <brunogcpinheiro@hotmail.com>
2018-08-16 20:11:03 -0300

    Saving users to database

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://plataforma-oam-vue-brunogcpinheiro.c9users.io:8080"
+  baseURL: "http://127.0.0.1:3333"
 });
 
 export default api;
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -6,7 +6,6 @@
             <div class="users-table">
               <div class="table-actions">
                 <h2>Tabela de Usuários</h2>
-                <v-form>
                 <v-dialog v-model="userDialog" max-width="500px">
                     <v-btn slot="activator" color="secondary"><v-icon>add</v-icon> Adicionar Usuário</v-btn>
                     <v-card>
@@ -16,8 +15,9 @@
                       <v-card-text>
                         <v-container grid-list-md>
                           <v-layout wrap>
+                            <v-form>
                             <v-flex xs12>
-                              <v-text-field 
+                              <v-text-field
                                   label="Usuário *"
                                   hint="Nome completo"
                                   v-model="username"
@@ -25,16 +25,16 @@
                               ></v-text-field>
                             </v-flex>
                             <v-flex xs12>
-                              <v-text-field 
-                                  label="Email *" 
+                              <v-text-field
+                                  label="Email *"
                                   hint="E-mail de compra"
                                   v-model="email"
                                   required
                               ></v-text-field>
                             </v-flex>
                             <v-flex xs12>
-                              <v-text-field 
-                                  label="Senha *" 
+                              <v-text-field
+                                  label="Senha *"
                                   type="password"
                                   v-model="password"
                                   required
@@ -45,7 +45,6 @@
                                 v-model="admin"
                                 label="Administrador"
                                 color="secondary"
-                                value="admin"
                                 hide-details
                               ></v-checkbox>
                             </v-flex>
@@ -57,18 +56,18 @@
                                 chips
                               ></v-autocomplete>
                             </v-flex>
+                            </v-form>
                           </v-layout>
                         </v-container>
                         <small>* Campos obrigatórios</small>
                       </v-card-text>
                       <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="secondary" @click.native="userDialog = false" type="submit">Salvar</v-btn>
+                        <v-btn color="secondary" @click.native="userDialog = false" type="submit" @click="onSubmit">Salvar</v-btn>
                         <v-btn color="error" @click.native="userDialog = false" type="submit">Fechar</v-btn>
                       </v-card-actions>
                     </v-card>
                   </v-dialog>
-                  </v-form>
               </div>
                 <v-layout>
                     <v-flex>
@@ -101,7 +100,7 @@
                     </v-flex>
                 </v-layout>
             </div>
-            
+
             <div class="courses-table">
               <div class="courses-actions">
                 <h2>Tabela de Cursos</h2>
@@ -115,7 +114,7 @@
                         <v-container grid-list-md>
                           <v-layout wrap>
                             <v-flex xs12>
-                              <v-text-field 
+                              <v-text-field
                                   label="Nome do curso *"
                                   required
                               ></v-text-field>
@@ -126,7 +125,7 @@
                       </v-card-text>
                       <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="secondary" @click.native="courseDialog = false">Salvar</v-btn>
+                        <v-btn color="secondary" @click.native="courseDialog = false" @click="onSubmit">Salvar</v-btn>
                         <v-btn color="error" @click.native="courseDialog = false">Fechar</v-btn>
                       </v-card-actions>
                     </v-card>
@@ -158,7 +157,7 @@
                         </template>
                       </v-data-table>
                     </v-flex>
-                </v-layout> 
+                </v-layout>
             </div>
         </v-container>
     </div>
@@ -189,6 +188,20 @@ export default {
             email: 'admin@admin.com.br',
             admin: true,
             cursos: '1, 2',
+          },
+          {
+            id: "2",
+            name: 'Bruno',
+            email: 'bruno@admin.com.br',
+            admin: true,
+            cursos: '4',
+          },
+          {
+            id: "3",
+            name: 'Renata',
+            email: 'renata@admin.com.br',
+            admin: false,
+            cursos: '1, 3',
           },
         ],
         coursesHeaders: [
@@ -222,7 +235,7 @@ export default {
           admin: this.admin,
         };
       console.log(registerData);
-        
+
       this.$store.dispatch('register', registerData);
       }
     },
@@ -231,29 +244,31 @@ export default {
 
 <style scoped>
 h1 {
-    margin-top: -30px;
-    text-align: center;
+  margin-top: -30px;
+  text-align: center;
 }
 
 .v-btn {
-    margin: 20px 10px 20px 0;
-    float: right;
+  margin: 20px 10px 20px 0;
+  float: right;
 }
 
 .v-icon {
-    margin-right: 5px;
+  margin-right: 5px;
 }
 
-.users-table, .courses-table {
+.users-table,
+.courses-table {
   margin: 25px 0 35px 0;
   border: 1px solid #e5e5e5;
   background: #d1d8e0;
-  -webkit-box-shadow: 0px 2px 5px 0px rgba(196,196,196,1);
-  -moz-box-shadow: 0px 2px 5px 0px rgba(196,196,196,1);
-  box-shadow: 0px 2px 5px 0px rgba(196,196,196,1);
+  -webkit-box-shadow: 0px 2px 5px 0px rgba(196, 196, 196, 1);
+  -moz-box-shadow: 0px 2px 5px 0px rgba(196, 196, 196, 1);
+  box-shadow: 0px 2px 5px 0px rgba(196, 196, 196, 1);
 }
 
-.table-actions, .courses-actions {
+.table-actions,
+.courses-actions {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -261,6 +276,6 @@ h1 {
 
 h2 {
   margin-left: 10px;
-  color: #182C61;
+  color: #182c61;
 }
 </style>
